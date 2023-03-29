1bfeb12f68b8350e86e1a803cb676a839e5348fa
kabirdutt0907 <duttnv@rknec.edu>
2023-03-29 10:38:15 +0530

    Stepper Section completed

--- src/components/results/Stepper/Steps.tsx
+++ src/components/results/Stepper/Steps.tsx
@@ -1,24 +1,33 @@
-import { Box } from '@mui/system';
-import React from 'react';
-import Stepper from '@mui/material/Stepper';
-import Step from '@mui/material/Step';
-import StepLabel from '@mui/material/StepLabel';
-import  { steps }  from '../../../constants/constStepText';
+import { Box } from "@mui/system";
+import React from "react";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
+import { steps } from "../../../constants/constStepText";
 import "./stepper.scss";
 
 const Steps = () => {
- 
   return (
-    <Box className = {"main-stepper-section"} >
-       <Stepper sx = {{"& .MuiSvgIcon-root": {color : 'white'} ,  width: '60%' } }activeStep={1} alternativeLabel>
-        {steps.map((label) => (
-          <Step sx={{}} className= {"step-size"} key={label}>
-            <StepLabel sx = {{"& .MuiStepLabel-label.MuiCompleted" : {color : 'white'}}} className= {"step-label"}>{label}</StepLabel>
+    <Box className={"main-stepper-section"}>
+      <Stepper
+        sx={{ "& .MuiSvgIcon-root": { color: "white" }, width: "31.5%"}}
+        activeStep={1}
+        alternativeLabel
+      >
+        {steps.map((label, index) => (
+          <Step sx={{}} className={"step-size"} key={label}>
+            <StepLabel
+              sx={{ "& .MuiStepLabel-label.MuiCompleted": { color: "white" } }}
+              className={"step-label"}
+            >
+              {label}
+            </StepLabel>
+            {index < steps.length - 1 && <div className="step-connector" />}
           </Step>
         ))}
       </Stepper>
     </Box>
-  )
-}
+  );
+};
 
 export default Steps;
