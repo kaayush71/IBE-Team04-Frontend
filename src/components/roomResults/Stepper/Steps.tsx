99ced31b699da02d6bd2d5d6dbe16181f571b6f0
kabirdutt0907 <duttnv@rknec.edu>
2023-04-10 10:09:52 +0530

    Minor changes in stepper

--- src/components/roomResults/Stepper/Steps.tsx
+++ src/components/roomResults/Stepper/Steps.tsx
@@ -6,15 +6,25 @@ import StepLabel from "@mui/material/StepLabel";
 import { steps } from "../../../constants/constStepText";
 import "./stepper.scss";
 import { useTranslation } from "react-i18next";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Steps = () => {
   const {t} = useTranslation();
-
+  const navigate = useNavigate();
   let currentLocation = useLocation();
   let stepNumber =1;
   if(currentLocation.pathname === "/checkout"){
     stepNumber = 2;
+  }
+  const handleClick = (label:string) => {
+    console.log("I am here");
+    console.log(label);
+    if(label === "1: Choose room"){
+      navigate("/room-search-results");
+    }
+    else if(label === "3: Checkout"){
+      navigate("/checkout");
+    }
   }
 
   return (
@@ -26,10 +36,11 @@ const Steps = () => {
         alternativeLabel
       >
         {steps.map((label, index) => (
-          <Step sx={{}} className={"step-size"} key={label}>
+          <Step  sx={{}} className={"step-size"} key={label}  onClick={() => handleClick(label)}>
             <StepLabel
               sx={{ "& .MuiStepLabel-label": { color: "white",marginTop:"0.1rem !important" }}}
               className={"step-label"}
+             
             >
               {t(label)}
             </StepLabel>
